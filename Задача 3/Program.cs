4258c73840e4c6c15ba751eb039a185a4ac283ec
Roman <brv161@gmail.com>
2022-09-11 15:41:20 +0300

    Задача 3

---
+++ Задача 3/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 7, 4, 9, 6, 1, 5, 3 };
+
+int n = array.Length;
+int find =5;
+
+int i = 0;
+
+while (i < n)
+{
+    if(array[i] == find)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+    //i = i + 1;
+    i++;
+}
